cba00f9275256a553fc28e5309425af1190e6162
gabriela.becker <gabriela.becker@angellira.com.br>
2024-12-12 09:19:11 -0300

    Criando Lista de Usuários

---
+++ src/data/Users.js
@@ -0,0 +1,32 @@
+export default [
+    {
+      id: 1,
+      name: 'João Silva',
+      email: 'joaosilva@email.com',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.jpg',
+    },
+    {
+      id: 2,
+      name: 'Mariana Aguiar',
+      email: 'marianaaguiar@email.com',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png',
+    },
+    {
+      id: 3,
+      name: 'Julio Fragoso',
+      email: 'juliofragoso@email.com',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+    },
+    {
+      id: 4,
+      name: 'Rafael Monteiro',
+      email: 'rafaelmonteiro@email.com',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    {
+      id: 5,
+      name: 'Ana Paula',
+      email: 'anapaula@email.com',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
+    },
+  ];
