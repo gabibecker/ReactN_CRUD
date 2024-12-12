c9bb090f132f8d947031f204aade029bba4baacb
gabriela.becker <gabriela.becker@angellira.com.br>
2024-12-12 08:45:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import UserList from "./View/UserList";
+import UserForm from "./View/UserForm";
+import { Button, Icon } from "react-native-elements";
+
+const Stack = createNativeStackNavigator()
+
+export default props => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="UserList"
+                screenOptions={screenOptions}>
+                <Stack.Screen
+                    name="UserList"
+                    component={UserList}
+                    options={() => {
+                      return {
+                        title: "Lista de Usuários",
+                        headerRight: () => {
+                            <Button
+                                type="clear"
+                                icon={<Icon name="add" size={25} color="white"/>}
+                                onPress={() => {
+                                    
+                                }}
+                            />
+                        }
+                      }  
+                    }}
+                />
+                <Stack.Screen
+                    name="UserForm"
+                    component={UserForm}
+                    options={{
+                        title: "Formulário de Usuários"
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#F4511e'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
+}
