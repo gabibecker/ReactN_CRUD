8fdee0290637b894fa41d37a29bf7b5198e55090
gabriela.becker <gabriela.becker@angellira.com.br>
2024-12-12 10:49:09 -0300

    Formulário de usuário

--- src/View/UserForm.js
+++ src/View/UserForm.js
@@ -1,9 +1,51 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Button } from "react-native-elements";
 
-export default props => {
-    const [user, setUser] = useState(props.route.params ? props.route.params : {})
+export default ({route, navigation}) => {
+    const [user, setUser] = useState(route.params ? route.params : {})
     return(
-        <Text>{user.id}</Text>
+        <View style = {style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChange={name => setUser({...user, name})}
+                placeholder="Informe o Nome"
+                value={ user.name}
+            />
+            <Text>E-mail</Text>
+            <TextInput
+                style={style.input}
+                onChange={email => setUser({...user, email})}
+                placeholder="Informe o E-mail"
+                value={ user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChange={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder="Informe o a URL do Avatar"
+                value={ user.avatarUrl}
+            />
+            <Button 
+                title={"Salvar"}
+                onPress={() => {
+                    navigation.goBack()
+                }}
+            />
+        </View>
+        
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 20
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 18,
+    }
+})
